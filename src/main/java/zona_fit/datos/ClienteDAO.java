61471d25f14fc77656a7545f39225174c5458dd5
Samuel <samuel.iza@outlook.com>
2025-07-02 20:31:59 -0600

    Creacion metodo buscarClientePorIDd

--- src/main/java/zona_fit/datos/ClienteDAO.java
+++ src/main/java/zona_fit/datos/ClienteDAO.java
@@ -44,6 +44,30 @@ public class ClienteDAO implements IClienteDAO{
 
     @Override
     public boolean buscarClientePorId(Cliente cliente) {
+        PreparedStatement ps;
+        ResultSet rs;
+        Connection con = getConexion();
+        String sql = "SELECT * FROM cliente WHERE id = ?";
+        try{
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, cliente.getId());
+            rs  = ps.executeQuery();
+            if (rs.next()){
+                cliente.setNombre(rs.getString("nombre"));
+                cliente.setApellido(rs.getString("apellido"));
+                cliente.setMembresia(rs.getInt("membresia"));
+                return true;
+            }
+        }catch (Exception e){
+            System.out.println("Error al buscar cliente por id: " + e.getMessage());
+        }
+        finally {
+            try{
+                con.close();
+            }catch (Exception e){
+                System.out.println("Error al cerrar la conexion: " + e.getMessage());
+            }
+        }
         return false;
     }
 
@@ -60,5 +84,26 @@ public class ClienteDAO implements IClienteDAO{
     @Override
     public boolean eliminarCliente(Cliente cliente) {
         return false;
+    }
+
+    public static void main(String[] args) {
+        IClienteDAO clienteDAO = new ClienteDAO();
+
+        //Prueba Listar Clientes
+//        System.out.println("*** Listar Clientes ***");
+//        IClienteDAO clienteDAO = new ClienteDAO();
+//        List<Cliente> clientes = clienteDAO.listarClientes();
+//        clientes.forEach(System.out::println);
+
+        //Prueba Buscar Por ID
+        System.out.println("*** Buscar Cliente Por ID ***");
+        Cliente cliente1 = new Cliente(2);
+        System.out.println("Cliente antes de la busqueda: " + cliente1);
+        boolean encontrado = clienteDAO.buscarClientePorId(cliente1);
+        if (encontrado){
+            System.out.println("Cliente despues de la busqueda: " + cliente1);
+        }else{
+            System.out.println("Cliente no encontrado" + cliente1.getId());
+        }
     }
 }
